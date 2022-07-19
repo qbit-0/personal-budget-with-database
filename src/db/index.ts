8f37123728f3e1edb652b315a3bede09bbe7edab
Duy Pham <duypham12241999@gmail.com>
2022-07-18 18:08:20 -0700

    Update db pool config

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,10 @@
 import { Pool, QueryResult } from "pg";
 
 const pool = new Pool({
-    user: "me",
-    host: "localhost",
+    user: "khlsnukjlshexg",
+    host: "ec2-54-87-179-4.compute-1.amazonaws.com",
     database: "postgresql-acute-87514",
-    password: "password",
+    password: "f5d83a018492e592d094dd93c2ed58f4779eff4bb472c3fd269ee26a438973a0",
     port: 5432,
 });
 
